05731bea87cd466ecd4cbc3997825e8dddd9a906
Ashwathi-25 <ashwathidiana@gmail.com>
2022-12-21 18:49:12 +0530

    Array

---
+++ Projectarray/src/array/ArrayDemo.java
@@ -0,0 +1,14 @@
+package array;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] marks = {100,90,80,70,60};
+		
+		System.out.println(marks[2]);
+
+	}
+
+}
